5e8b976107269cfcf73592b55ba066d0cc6fda97
Isha Hulgeri <hulgeri.isha@gmail.com>
2024-04-25 02:34:36 +0530

    added loading gif to site

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -14,14 +15,16 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
@@ -29,7 +32,9 @@ export class News extends Component {
     console.log("Previous");
     if (this.state.page > 1) {
       let prevPage = this.state.page - 1;
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=${prevPage}&pageSize=20`;
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=${prevPage}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
@@ -37,6 +42,7 @@ export class News extends Component {
       this.setState({
         page: prevPage,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -44,10 +50,11 @@ export class News extends Component {
   handleNextClick = async () => {
     console.log("Next");
     const nextPage = this.state.page + 1;
-    const totalPages = Math.ceil(this.state.totalResults / 20); // Calculate total pages
+    const totalPages = Math.ceil(this.state.totalResults / this.props.pageSize); // Calculate total pages
 
-    if (nextPage <= totalPages) {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=${nextPage}&pageSize=20`;
+    if (!nextPage <= totalPages) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=f1e66d62082d4df591269b61f8e04aa0&page=${nextPage}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
@@ -55,6 +62,7 @@ export class News extends Component {
       this.setState({
         page: nextPage,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -62,22 +70,26 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>DailyNews - Top Headlines</h2>
+        <h1 className="text-center">DailyNews - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 88) : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -89,6 +101,10 @@ export class News extends Component {
             &larr; Previous
           </button>
           <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
             type="button"
             className="btn btn-dark"
             onClick={this.handleNextClick}
